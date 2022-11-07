d6aac661a1399697ca21d66df15ac4c70170eaf2
Rehman Shawon <rehman.shawon@apsissolutions.comm>
2022-11-07 13:23:04 +0600

    user name added to jwt payload

--- src/modules/sys-auth/sys-auth.service.ts
+++ src/modules/sys-auth/sys-auth.service.ts
@@ -32,7 +32,12 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { email: user.email, sub: user.id, role: user.role_id };
+    const payload = {
+      name: user.user_name,
+      email: user.email,
+      sub: user.id,
+      role: user.role_id,
+    };
     console.log(payload);
     return {
       access_token: this.jwtService.sign(payload),
